1fad83b90314bed9ffd04b8f9425478ce5c1c5c6
AJ Spivey <spivey.andrew@gmail.com>
2020-07-23 16:48:32 -0700

    Added variables z, w, y, and h. Added constraints for z, w, and h.

--- BP.py
+++ BP.py
@@ -5,8 +5,12 @@
 from docplex.mp.model import Model
 from itertools import permutations
 
+
+# ==============================================================================
+# FUNCTIONS
+# ==============================================================================
 def getPlacements():
-    return list(permutations(defenders + ([0] * (targetNum - len(defenders)))))
+    return list(permutations(defenders + ([-1] * (targetNum - len(defenders)))))
 
 def getLambdaPlacements():
     lambdaPlacements = []
@@ -15,16 +19,25 @@ def getLambdaPlacements():
             lambdaPlacements.append((aType, s))
     return lambdaPlacements
 
+def utilityK(s,k):
+    utility = 0 # Defended
+    if s[k] == -1: # Undefended
+        utility = -1 * sum([penalty[k] for penalty in dPenalties.values()])
+    return utility
+
+# ==============================================================================
+# GAME SETTINGS
+# ==============================================================================
 # Game and optimization
 targetNum = 3
-m = 1000
+m = 9999
 # Attackers
-aTypes = [1,2]
+aTypes = [0,1]
 aRewards = {1:[1,7,3], 2:[2,1,5]}
 aPenalties = {1:[2,2,2], 2:[1,1,2]}
 q = [0.7, 0.3]
 # Defenders
-defenders = [1,2]
+defenders = [0,1]
 dRewards = {1:[0,0,0],2:[0,0,0]}
 dPenalties = {1:[1,2,3],2:[3,1,1]}
 dCosts = {1:[1,1,2],2:[1,1,1]}
@@ -33,19 +46,36 @@ dCosts = {1:[1,1,2],2:[1,1,1]}
 placements = getPlacements()
 lambdaPlacements = getLambdaPlacements()
 
+# ==============================================================================
+# LP Definition & Constraints
+# ==============================================================================
 # Create the model and objective function
 model = Model('BayesianPersuasionSolver')
-z = model.continuous_var_dict(keys=lambdaPlacements, ub=2)
-objectiveFunction = sum([q[lam - 1] * sum([z[(lam,s)] for s in placements]) for lam in aTypes])
+z = model.continuous_var_dict(keys=lambdaPlacements, name="z")
+w = model.continuous_var_dict(keys=lambdaPlacements, lb=0, ub=1, name="w")
+y = model.continuous_var_dict(keys=[(lam,s,m,i) for lam in aTypes for s in placements for m in defenders for i in range(targetNum)])
+h = model.binary_var_dict(keys=[(lam, k) for lam in aTypes for k in range(targetNum)], name="h")
+
+objectiveFunction = sum([q[lam] * sum([z[(lam,s)] for s in placements]) for lam in aTypes])
 
 # Add the constraints
+# Z constraints
+model.add_constraints([z[(lam,s)] <= w[(lam,s)] * utilityK(s,k) + (1 - h[(lam, k)])*m for k in range(targetNum) for s in placements for lam in aTypes])
+# W constraints
+model.add_constraints([sum([w[(lam,s)] for s in placements]) == 1 for lam in aTypes])
+# Y constraints
+# V constraints
+# H constraints -- already a binary variable
+model.add_constraints([sum([h[(lam,k)] for k in range(targetNum)]) == 1 for lam in aTypes])
 
 # Solve the problem
 model.maximize(objectiveFunction)
 model.solve()
+model.export("model.lp")
 print(model.get_solve_status())
 print(model.solution.get_objective_value())
 print([float(value) for value in z.values()])
+print([float(value) for value in h.values()])
 
 # Decision variables:
 #   omega -- depends on S and lambda
