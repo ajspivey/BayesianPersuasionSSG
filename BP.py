ff94848c354dd0764597d7fdae186451bf66dbce
AJ Spivey <spivey.andrew@gmail.com>
2020-07-24 10:50:36 -0700

    Added V constraints

--- BP.py
+++ BP.py
@@ -61,6 +61,7 @@ z = model.continuous_var_dict(keys=lambdaPlacements, name="z")
 w = model.continuous_var_dict(keys=lambdaPlacements, lb=0, ub=1, name="w")
 y = model.continuous_var_dict(keys=[(lam,s,m,i) for lam in aTypes for s in placements for m in defenders for i in range(targetNum)], name="y")
 h = model.binary_var_dict(keys=[(lam, k) for lam in aTypes for k in range(targetNum)], name="h")
+v = model.continuous_var_dict(keys=aTypes, name="v")
 
 objectiveFunction = sum([q[lam] * sum([z[(lam,s)] for s in placements]) for lam in aTypes])
 
@@ -74,6 +75,8 @@ model.add_constraints([sum(q[lam] * sum([y[(lam,s,m,i)] for s in placements if s
 model.add_constraints([y[(lam,s,m,i)] >= w[(lam,s)]  * utilityM(s,i,k,m) - (1-h[(lam,k)]) * M for s in placements for lam in aTypes for i in range(targetNum) for k in range(targetNum) for m in defenders])
 model.add_constraints([y[(lam,s,m,i)] <= w[(lam,s)]  * utilityM(s,i,k,m) + (1-h[(lam,k)]) * M for s in placements for lam in aTypes for i in range(targetNum) for k in range(targetNum) for m in defenders])
 # V constraints
+model.add_constraints([v[lam] >= sum([w[(lam,s)] for s in placements if s[i] != -1])*(dPenalties[lam][i] - dRewards[lam][i]) + dRewards[lam][i] for i in range(targetNum) for lam in aTypes])
+model.add_constraints([v[lam] <= sum([w[(lam,s)] for s in placements if s[i] != -1])*(dPenalties[lam][i] - dRewards[lam][i]) + dRewards[lam][i] + (1 - h[(lam,i)])*M for i in range(targetNum) for lam in aTypes])
 # H constraints -- already a binary variable
 model.add_constraints([sum([h[(lam,k)] for k in range(targetNum)]) == 1 for lam in aTypes])
 
@@ -83,35 +86,3 @@ model.solve()
 model.export("model.lp")
 print(model.get_solve_status())
 print(model.solution.get_objective_value())
-
-# Decision variables:
-#   omega -- depends on S and lambda
-#   v --
-#   h -- dpends on lambda and k
-#   y -- depends on m and s and lambda and i
-#   z -- depends on S and lambda
-#
-
-
-# # Create a cplex model
-# model = Model('BayesianPersuasionSolver')
-# # Define the decision variables
-# sW = mD.continuous_var(lb=float("-inf"),name='socialWelfare')
-# xD = mD.continuous_var_dict(keys=dIds)
-# # Constraints
-# mD.add_constraint(sum(xD.values()) == 1)
-# mD.add_constraints(xVal <= 1 for xVal in xD.values())
-# mD.add_constraints(xVal >= 0 for xVal in xD.values())
-# mD.add_constraints(vD <= sum([xD[dId] * payoutMatrix[dId, aId] for dId in dIds]) for aId in aIds)
-#
-# mD.maximize(vD)
-# if export:
-#     mD.export("defenderModel.lp")
-# return mD, xD, vD
-
-# def getDefenderMixedStrategy(dIds, dMap, aIds, aMap, payoutMatrix, export=False):
-#     defenderModel, dStrategyDistribution, dUtility = createDefenderModel(dIds, dMap, aIds, aMap, payoutMatrix, export)
-#     defenderModel.solve()
-#     defenderMixedStrategy = [float(value) for value in dStrategyDistribution.values()]
-#     dUtility = float(dUtility)
-#     return defenderMixedStrategy, dUtility
