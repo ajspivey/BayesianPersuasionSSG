34b1bb7cad009ce33fdf1e15e2c9ca564b5da941
AJ Spivey <spivey.andrew@gmail.com>
2020-07-28 14:33:29 -0700

    Set lower bounds for decision variables

--- BP.py
+++ BP.py
@@ -22,13 +22,13 @@ def getLambdaPlacements():
 def utilityK(s,k):
     utility = 0 # Defended
     if s[k] == -1: # Undefended
-        utility = -1 * sum([penalty[k] for penalty in dPenalties.values()])
+        utility = sum([penalty[k] for penalty in dPenalties.values()])
     return utility
 
 def utilityM(s,i,k,m):
     utility = 0 # Defended
     if s[k] == -1 or (s[k] == m and i != k): # Undefended
-        utility = -1 * dPenalties[m][k]
+        utility = sum([penalty[k] for penalty in dPenalties.values()])
     return utility
 
 # ==============================================================================
@@ -38,15 +38,15 @@ def utilityM(s,i,k,m):
 targetNum = 3
 M = 9999
 # Attackers
-aTypes = [0,1]
-aRewards = {0:[1,7,3], 1:[2,1,5]}
-aPenalties = {0:[2,2,2], 1:[1,1,2]}
+aTypes = [1,2]
+aRewards = {1:[1,7,3], 2:[2,1,5]}
+aPenalties = {1:[-2,-2,-2], 2:[-1,-1,-2]}
 q = [0.7, 0.3]
 # Defenders
-defenders = [0,1]
-dRewards = {0:[0,0,0],1:[0,0,0]}
-dPenalties = {0:[1,2,3],1:[3,1,1]}
-dCosts = {0:[1,1,2],1:[1,1,1]}
+defenders = [1,2]
+dRewards = {1:[0,0,0],2:[0,0,0]}
+dPenalties = {1:[-1,-2,-3],2:[-3,-1,-1]}
+dCosts = {1:[1,1,2],2:[1,1,1]}
 # Create the list of lambda placement keys -- all possible assignments, with each defender
 # type attached.
 placements = getPlacements()
@@ -57,26 +57,26 @@ lambdaPlacements = getLambdaPlacements()
 # ==============================================================================
 # Create the model and objective function
 model = Model('BayesianPersuasionSolver')
-z = model.continuous_var_dict(keys=lambdaPlacements, name="z")
+z = model.continuous_var_dict(keys=lambdaPlacements, lb=-model.infinity, name="z")
 w = model.continuous_var_dict(keys=lambdaPlacements, lb=0, ub=1, name="w")
-y = model.continuous_var_dict(keys=[(lam,s,m,i) for lam in aTypes for s in placements for m in defenders for i in range(targetNum)], name="y")
-h = model.binary_var_dict(keys=[(lam, k) for lam in aTypes for k in range(targetNum)], name="h")
-v = model.continuous_var_dict(keys=aTypes, name="v")
+y = model.continuous_var_dict(keys=[(lam,s,m,i) for lam in aTypes for s in placements for m in defenders for i in range(targetNum)], lb=-model.infinity, name="y")
+h = model.binary_var_dict(keys=[(lam, k) for lam in aTypes for k in range(targetNum)], lb=0, ub=1, name="h")
+v = model.continuous_var_dict(keys=aTypes, lb=-model.infinity, name="v")
 
-objectiveFunction = sum([q[lam] * sum([z[(lam,s)] for s in placements]) for lam in aTypes])
+objectiveFunction = sum([q[lam - 1] * sum([z[(lam,s)] for s in placements]) for lam in aTypes])
 
 # Add the constraints
 # Z constraints
 model.add_constraints([z[(lam,s)] <= w[(lam,s)] * utilityK(s,k) + (1 - h[(lam, k)])*M for k in range(targetNum) for s in placements for lam in aTypes])
-# W constraints
-model.add_constraints([sum([w[(lam,s)] for s in placements]) == 1 for lam in aTypes])
 # Y constraints
-model.add_constraints([sum(q[lam] * sum([y[(lam,s,m,i)] for s in placements if s[i] == m]) for lam in aTypes) >= sum(q[lam] * sum([y[(lam,s,m,j)] for s in placements if s[i] == m]) for lam in aTypes) for i in range(targetNum) for j in range(targetNum) if j != i for m in defenders])
+model.add_constraints([sum(q[lam - 1] * sum([y[(lam,s,m,i)] for s in placements if s[i] == m]) for lam in aTypes) >= sum(q[lam - 1] * sum([y[(lam,s,m,j)] for s in placements if s[i] == m]) for lam in aTypes) for i in range(targetNum) for j in range(targetNum) if j != i for m in defenders])
 model.add_constraints([y[(lam,s,m,i)] >= w[(lam,s)]  * utilityM(s,i,k,m) - (1-h[(lam,k)]) * M for s in placements for lam in aTypes for i in range(targetNum) for k in range(targetNum) for m in defenders])
 model.add_constraints([y[(lam,s,m,i)] <= w[(lam,s)]  * utilityM(s,i,k,m) + (1-h[(lam,k)]) * M for s in placements for lam in aTypes for i in range(targetNum) for k in range(targetNum) for m in defenders])
+# W constraints
+model.add_constraints([sum([w[(lam,s)] for s in placements]) == 1 for lam in aTypes])
 # V constraints
-model.add_constraints([v[lam] >= sum([w[(lam,s)] for s in placements if s[i] != -1])*(dPenalties[lam][i] - dRewards[lam][i]) + dRewards[lam][i] for i in range(targetNum) for lam in aTypes])
-model.add_constraints([v[lam] <= sum([w[(lam,s)] for s in placements if s[i] != -1])*(dPenalties[lam][i] - dRewards[lam][i]) + dRewards[lam][i] + (1 - h[(lam,i)])*M for i in range(targetNum) for lam in aTypes])
+model.add_constraints([v[lam] >= sum([w[(lam,s)] for s in placements if s[i] != -1])*(aPenalties[lam][i] - aRewards[lam][i]) + aRewards[lam][i] for i in range(targetNum) for lam in aTypes])
+model.add_constraints([v[lam] <= sum([w[(lam,s)] for s in placements if s[i] != -1])*(aPenalties[lam][i] - aRewards[lam][i]) + aRewards[lam][i] + (1 - h[(lam,i)])*M for i in range(targetNum) for lam in aTypes])
 # H constraints -- already a binary variable
 model.add_constraints([sum([h[(lam,k)] for k in range(targetNum)]) == 1 for lam in aTypes])
 
@@ -86,3 +86,4 @@ model.solve()
 model.export("model.lp")
 print(model.get_solve_status())
 print(model.solution.get_objective_value())
+print(list([float(wVal) for wVal in w.values()]))
