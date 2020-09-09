7a1f690ad74b8ea27630d0f28be37320694ec2c3
AJ Spivey <spivey.andrew@gmail.com>
2020-09-09 16:31:37 -0700

    Added Relaxed file, BPCompact WIP (check notes)

--- BPRelaxed.py
+++ BPRelaxed.py
@@ -51,8 +51,65 @@ def generateRandomAttackers(attackerNum, targetNum, rewardCeiling=20, penaltyCei
             aPenalties[a].append(-1 * randThing.randint(1,penaltyCeiling))
     return attackers, aRewards, aPenalties, q
 
-def getOmegaKeys(defenders, targetNum, aTypes):
-    return [(m,i,lam) for m in defenders for i in rage(targetNum) for lam in aTypes]
+def numberToBase(n, b, length):
+    if n == 0:
+        answer = [0] * length
+        return tuple(answer)
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    answer = digits[::-1]
+    if len(answer) < length:
+        for i in range(length - len(answer)):
+            answer.append(0)
+    return tuple(answer)
+
+def getPlacements(defenders, targetNum):
+    return list(set([numberToBase(i,targetNum,len(defenders)) for i in range(targetNum ** len(defenders))]))
+
+def getOmegaKeys():
+    omegaKeys = []
+    for aType in aTypes:
+        for s in placements:
+            for a in attackerActions:
+                omegaKeys.append((s, a, aType))
+    return list(set(omegaKeys))
+
+def defenderSocialUtility(s,k):
+    utility = 0
+    defended = False
+    for targetIndex in s:
+        if targetIndex == k:
+            defended = True
+    for defender in defenders:
+        targetIndex = s[defender]
+        utility += dCosts[defender][targetIndex]
+        if defended == False:
+            utility += dPenalties[defender][targetIndex]
+    return utility
+
+def utilityM(d,dm,a,m):
+    utility = dCosts[m][d]
+    defended = False
+    for dIndex in range(len(dm)):
+        if dIndex != m and dm[dIndex] == a:
+            defended = True
+    if d == a:
+        defended = True
+    if defended == False:
+        utility += dPenalties[m][d]
+    return utility
+
+def aUtility(s,a,lam):
+    utility = aRewards[lam][a]
+    defended = False
+    for targetIndex in s:
+        if targetIndex == a:
+            defended = True
+    if defended == True:
+        utility = aPenalties[lam][a]
+    return utility
 
 start_time = getTime()
 # ==============================================================================
@@ -64,15 +121,25 @@ attackerNum = 3
 M = 1000
 defenders, dRewards, dPenalties, dCosts = generateRandomDefenders(defenderNum, targetNum)
 aTypes, aRewards, aPenalties, q = generateRandomAttackers(attackerNum, targetNum)
-omegaKeys = getOmegaKeys(defenders,targetNum, aTypes)
+placements = getPlacements(defenders, targetNum)
+attackerActions = list(range(targetNum))
+omegaKeys = getOmegaKeys()
 
 # ==============================================================================
 # LP Definition & Constraints
 # ==============================================================================
 # Create the model and objective function
-model = Model('BayesianPersuasionSolverCompact')
+model = Model('BayesianPersuasionSolver')
 w = model.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
-model.add_constraints([] <= 1 for i in range(targetNum)], names=[f"{a}"])
+
+objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a) for s in placements for a in attackerActions]) for lam in aTypes])
+# Add the constraints
+# W constraints
+model.add_constraints([sum([w[(s,a,lam)] * aUtility(s,a,lam) for s in placements]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam) for s in placements]) for a in attackerActions for b in attackerActions if a != b for lam in aTypes], names=[f"c att {lam} suggested {a}, but goes to {b}" for a in attackerActions for b in attackerActions if a != b for lam in aTypes])
+model.add_constraints([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m) for a in attackerActions for dm in placements if dm[m] == d])]) >= \
+                       sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m) for a in attackerActions for dm in placements if dm[m] == d])]) \
+                       for m in defenders for d in range(targetNum) for e in range(targetNum) if d != e for lam in aTypes])
+model.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
 # Solve the problem
 model.maximize(objectiveFunction)
 model.solve()
