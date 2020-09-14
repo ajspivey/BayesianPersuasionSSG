807269deb8b72de9c7ff0a3ff3c0237251fcbe9a
AJ Spivey <spivey.andrew@gmail.com>
2020-09-14 13:53:29 -0700

    fixed relaxed constraints formulation for coarse correlated equilibrium

--- BPRelaxed.py
+++ BPRelaxed.py
@@ -62,11 +62,11 @@ def numberToBase(n, b, length):
     answer = digits[::-1]
     if len(answer) < length:
         for i in range(length - len(answer)):
-            answer.append(0)
+            answer.insert(0,0)
     return tuple(answer)
 
 def getPlacements(defenders, targetNum):
-    return list(set([numberToBase(i,targetNum,len(defenders)) for i in range(targetNum ** len(defenders))]))
+    return [numberToBase(i,targetNum,len(defenders)) for i in range(targetNum ** len(defenders))]
 
 def getOmegaKeys():
     omegaKeys = []
@@ -120,7 +120,12 @@ defenderNum = 2
 attackerNum = 1
 M = 1000
 defenders, dRewards, dPenalties, dCosts = generateRandomDefenders(defenderNum, targetNum)
+dRewards = {0:[0,0,0], 1:[0,0,0]}
+dPenalties = {0:[-8,-6,-11], 1:[-5,-17,-8]}
+dCosts = {0:[-10,-9,-1], 1:[-8,-5,-7]}
 aTypes, aRewards, aPenalties, q = generateRandomAttackers(attackerNum, targetNum)
+aRewards = {0:[5,1,10]}
+aPenalties = {0:[-17,-18,-11]}
 placements = getPlacements(defenders, targetNum)
 attackerActions = list(range(targetNum))
 omegaKeys = getOmegaKeys()
@@ -134,16 +139,37 @@ w = model.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
 
 objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a) for s in placements for a in attackerActions]) for lam in aTypes])
 # Constraints
-model.add_constraints([sum([sum([w[(s,a,lam)] * aUtility(s,a,lam) for s in placements]) for a in attackerActions]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam) for s in placements for a in attackerActions]) for b in attackerActions for lam in aTypes], names=[f"c att {lam} ignores all suggestions and always goes to {b}" for b in attackerActions for lam in aTypes])
-model.add_constraints([sum([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m) for a in attackerActions for dm in placements if dm[m] == d])]) for d in range(targetNum)]) >= \
-                       sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m) for a in attackerActions for d in range(targetNum) for dm in placements if dm[m] == d])]) \
-                       for m in defenders for e in range(targetNum) for lam in aTypes], names=[f"defender {m} ignores all suggestions and goes to {e} every time" for m in defenders for e in range(targetNum) for lam in aTypes])
+model.add_constraints([sum([sum([w[(s,a,lam)] * aUtility(s,a,lam) for s in placements]) for a in attackerActions]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam) for s in placements for a in attackerActions]) for lam in aTypes for b in attackerActions], names=[f"attacker {lam} goes to {b}" for lam in aTypes for b in attackerActions])
+model.add_constraints([sum([q[lam] * sum([sum([w[(dm,a,lam)] * utilityM(d,dm,a,m) for dm in placements if dm[m] == d for a in attackerActions]) for d in range(targetNum) ]) for lam in aTypes]) >= \
+                        sum([ q[lam] * sum([ w[(dm,a,lam)] * utilityM(e,dm,a,m) for a in attackerActions for d in range(targetNum) for dm in placements if dm[m] == d]) for lam in aTypes])
+                        for m in defenders for e in range(targetNum) for lam in aTypes], names=[f"defender {m} to {e}" for m in defenders for e in range(targetNum) for lam in aTypes])
 model.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
 # Solve the problem
 model.maximize(objectiveFunction)
 model.solve()
-model.export("exampleModel.lp")
+model.export("relaxedModel.lp")
 print("--- %s seconds ---" % (getTime() - start_time))
 print(model.get_solve_status())
-for k, v in w.items():
-    print(k, float(v))
+# for k, v in w.items():
+#     print(k, float(v))
+
+for _ in range(100):
+    print(f"Run {_} of 100")
+    defenders, dRewards, dPenalties, dCosts = generateRandomDefenders(defenderNum, targetNum)
+    aTypes, aRewards, aPenalties, q = generateRandomAttackers(attackerNum, targetNum)
+    placements = getPlacements(defenders, targetNum)
+    attackerActions = list(range(targetNum))
+    omegaKeys = getOmegaKeys()
+    model = Model('BayesianPersuasionSolver')
+    w = model.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
+
+    objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a) for s in placements for a in attackerActions]) for lam in aTypes])
+    # Constraints
+    model.add_constraints([sum([sum([w[(s,a,lam)] * aUtility(s,a,lam) for s in placements]) for a in attackerActions]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam) for s in placements for a in attackerActions]) for lam in aTypes for b in attackerActions], names=[f"attacker {lam} goes to {b}" for lam in aTypes for b in attackerActions])
+    model.add_constraints([sum([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m) for a in attackerActions for dm in placements if dm[m] == d])]) for d in range(targetNum)]) >= \
+                           sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m) for a in attackerActions for d in range(targetNum) for dm in placements if dm[m] == d])]) \
+                           for m in defenders for e in range(targetNum) for lam in aTypes], names=[f"defender {m} ignores all suggestions and goes to {e} every time" for m in defenders for e in range(targetNum) for lam in aTypes])
+    model.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
+    # Solve the problem
+    model.maximize(objectiveFunction)
+    model.solve()
