f08cd2ad66f0097e1a3b8279c7a4e8847793058a
AJ Spivey <spivey.andrew@gmail.com>
2020-10-05 21:52:33 -0700

    Added time budget and bpNRDA calculations. full comparison for targets

--- Comparison.py
+++ Comparison.py
@@ -135,10 +135,11 @@ def probabilityProtected(dStrats, targetNum):
         protectionOdds.append(1-reduce(mul, [1-odd for odd in probabilities]))
     return protectionOdds
 
-def createGraph(title, xLabel, yLabel, v1, v1Label, v2, v2Label):
+def createGraph(title, xLabel, yLabel, v1, v1Label, v2, v2Label, v3, v3Label, xStart=1):
     g = plt.figure()
-    plt.plot(range(1, len(v1) + 1), v2, 'r', label=f'{v2Label}')
-    plt.plot(range(1, len(v1) + 1), v1, 'g', label=f'{v1Label}')
+    plt.plot(range(xStart, len(v3) + xStart), v3, 'r', label=f'{v3Label}')
+    plt.plot(range(xStart, len(v2) + xStart), v2, 'b', label=f'{v2Label}')
+    plt.plot(range(xStart, len(v1) + xStart), v1, 'g', label=f'{v1Label}')
     plt.title(f"{title}")
     plt.xlabel(f"{xLabel}")
     plt.ylabel(f"{yLabel}")
@@ -146,9 +147,90 @@ def createGraph(title, xLabel, yLabel, v1, v1Label, v2, v2Label):
     plt.savefig(f"./{title}.png")
     return g
 
-def getAvgUtilitiesAndTimes(targetNum, avgCount=10):
+def solveBPAllowOverlap(targetNum, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q):
+    placements = getPlacements(defenders, targetNum)
+    attackerActions = list(range(targetNum))
+    omegaKeys = getOmegaKeys(aTypes, placements, attackerActions)
+    model = Model('BayesianPersuasionSolverWithOverlap')
+    w = model.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
+    objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a,defenders, dCosts, dPenalties) for s in placements for a in attackerActions]) for lam in aTypes])
+    model.add_constraints([sum([w[(s,a,lam)] * aUtility(s,a,lam, aPenalties, aRewards) for s in placements]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam, aPenalties, aRewards) for s in placements]) for a in attackerActions for b in attackerActions if a != b for lam in aTypes], names=[f"c att {lam} suggested {a}, but goes to {b}" for a in attackerActions for b in attackerActions if a != b for lam in aTypes])
+    model.add_constraints([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m, dRewards, dPenalties, dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) >= \
+                           sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m, dRewards, dPenalties, dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) \
+                           for m in defenders for d in range(targetNum) for e in range(targetNum) if d != e for lam in aTypes])
+    model.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
+    model.maximize(objectiveFunction)
+    model.solve()
+    return model.solution.get_objective_value()
+
+def solveBPNoRequiredDefenderAssignment(targetNum, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q):
+    _dRewards = dRewards.copy()
+    _dPenalties = dPenalties.copy()
+    _dCosts = dCosts.copy()
+    for m in defenders:
+        _dRewards[m].append(0)
+        _dPenalties[m].append(0)
+        _dCosts[m].append(0)
+    placements = getPlacements(defenders, targetNum + 1)
+    attackerActions = list(range(targetNum))
+    omegaKeys = getOmegaKeys(aTypes, placements, attackerActions)
+    model = Model('BayesianPersuasionSolverWithoutRequiredAssignment')
+    w = model.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
+    objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a,defenders, _dCosts, _dPenalties) for s in placements for a in attackerActions]) for lam in aTypes])
+    model.add_constraints([sum([w[(s,a,lam)] * aUtility(s,a,lam, aPenalties, aRewards) for s in placements]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam, aPenalties, aRewards) for s in placements]) for a in attackerActions for b in attackerActions if a != b for lam in aTypes], names=[f"c att {lam} suggested {a}, but goes to {b}" for a in attackerActions for b in attackerActions if a != b for lam in aTypes])
+    model.add_constraints([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m, _dRewards, _dPenalties, _dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) >= \
+                           sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m, _dRewards, _dPenalties, _dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) \
+                           for m in defenders for d in range(targetNum + 1) for e in range(targetNum + 1) if d != e for lam in aTypes])
+    model.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
+    model.maximize(objectiveFunction)
+    model.solve()
+    return model.solution.get_objective_value()
+
+def solveBaseline(targetNum, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q):
+    placements = getPlacements(defenders, targetNum)
+    attackerActions = list(range(targetNum))
+    omegaKeys = getOmegaKeys(aTypes, placements, attackerActions)
+    baselineUtility = 0
+    dStrat = {}
+    models2 = {}
+    for m in defenders:
+        model2 = Model(f"defenderStrategy{m}")
+        x = model2.continuous_var_list(keys=targetNum, lb=0, ub=1, name=f"x{m}")
+        h = model2.binary_var_dict(keys=[(lam, k) for lam in aTypes for k in range(targetNum)], lb=0, ub=1, name=f"h{m}")
+        ul = model2.continuous_var_dict(keys=aTypes, lb=-model2.infinity, name=f"ua{m}")
+        ud = model2.continuous_var_dict(keys=[lam for lam in aTypes], lb=-model2.infinity, name=f"ud{m}")
+        objectiveFunction = sum([q[lam] * ud[lam] for lam in aTypes])
+        model2.add_constraints([ud[lam] <= utilityDI(m,x,i,dRewards,dPenalties,dCosts) + (1-h[(lam,i)]) * M for i in range(targetNum) for lam in aTypes], names=[f"defender utility for lam {lam}, i {i}" for i in range(targetNum) for lam in aTypes])
+        model2.add_constraints([ul[lam] <= utilityLamI(x,lam,i,aRewards,aPenalties) + (1-h[(lam,i)]) * M for i in range(targetNum) for lam in aTypes], names=[f"lam {lam} utility leq for i {i}" for i in range(targetNum) for lam in aTypes])
+        model2.add_constraints([ul[lam] >= utilityLamI(x,lam,i,aRewards,aPenalties) for i in range(targetNum) for lam in aTypes], names=[f"lam {lam} utility geq, for i {i}" for i in range(targetNum) for lam in aTypes])
+        model2.add_constraints([sum([h[(lam,i)] for i in range(targetNum)]) == 1 for lam in aTypes], names=[f"h sum is 1 for lam {lam}" for lam in aTypes])
+        model2.add_constraint(sum([x[i] for i in range(targetNum)]) == 1)
+        # Solve the problem
+        model2.maximize(objectiveFunction)
+        model2.solve()
+        model2.export(f"modelBaseline{m}.lp")
+        dStrat[m] = list([float(xVal) for xVal in x])
+        models[m] = model2
+    # Attacker response
+    aStrat = 0
+    protectionOdds = probabilityProtected(dStrat, targetNum)
+    for lam in aTypes:
+        expectedUtilities = []
+        for i in range(targetNum):
+            expectedUtilities.append(((1-protectionOdds[i])*aRewards[lam][i]) + (protectionOdds[i]*aPenalties[lam][i]))
+        aStrat = argmax(expectedUtilities)
+    for m in defenders:
+        baselineUtility += dStrat[m][aStrat] * (dRewards[m][aStrat] + dCosts[m][aStrat]) + (1-dStrat[m][aStrat]) * (dPenalties[m][aStrat] + dCosts[m][aStrat])
+    return baselineUtility
+
+def iterateTargets(targetNum, avgCount, bpOverbudget, bpNRDAOverbudget, baselineOverbudget):
+    bpBelowBudget = True
+    bpNRDABelowBudget = True
+    baselineBelowBudget = True
     bpUtility = 0
     bpTime = 0
+    bpNRDAUtility = 0
+    bpNRDATime = 0
     baselineUtility = 0
     baselineTime = 0
     for _ in range(avgCount):
@@ -156,97 +238,48 @@ def getAvgUtilitiesAndTimes(targetNum, avgCount=10):
         # Generate a new game
         defenders, dRewards, dPenalties, dCosts = generateRandomDefenders(DEFENDERNUM, targetNum)
         aTypes, aRewards, aPenalties, q = generateRandomAttackers(ATTACKERNUM, targetNum)
-        placements = getPlacements(defenders, targetNum)
-        attackerActions = list(range(targetNum))
-        omegaKeys = getOmegaKeys(aTypes, placements, attackerActions)
-        # Build the BP Model
-        bpStart = getTime()
-        model = Model('BayesianPersuasionSolver')
-        w = model.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
-        objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a,defenders, dCosts, dPenalties) for s in placements for a in attackerActions]) for lam in aTypes])
-        model.add_constraints([sum([w[(s,a,lam)] * aUtility(s,a,lam, aPenalties, aRewards) for s in placements]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam, aPenalties, aRewards) for s in placements]) for a in attackerActions for b in attackerActions if a != b for lam in aTypes], names=[f"c att {lam} suggested {a}, but goes to {b}" for a in attackerActions for b in attackerActions if a != b for lam in aTypes])
-        model.add_constraints([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m, dRewards, dPenalties, dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) >= \
-                               sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m, dRewards, dPenalties, dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) \
-                               for m in defenders for d in range(targetNum) for e in range(targetNum) if d != e for lam in aTypes])
-        model.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
-        model.maximize(objectiveFunction)
-        model.solve()
-        bpUtility += model.solution.get_objective_value()
-        bpTime += getTime() - bpStart
 
+        # BP Allow Overlap
+        if not bpOverbudget:
+            bpStart = getTime()
+            bpUtility += solveBPAllowOverlap(targetNum, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q)
+            bpTime += getTime() - bpStart
+        else:
+            bpUtility = None
+            bpTime = None
 
-        # ================================
-        # BP Model with extra empty target
-        # ================================
-        for m in defenders:
-            dRewards[m].append(0)
-            dPenalties[m].append(0)
-            dCosts[m].append(0)
-        placements = getPlacements(defenders, targetNum + 1)
-        omegaKeys = getOmegaKeys(aTypes, placements, attackerActions)
-        baselineStart = getTime()
-        model2 = Model('BayesianPersuasionSolver')
-        w = model2.continuous_var_dict(keys=omegaKeys, lb=0, ub=1, name="w")
-        objectiveFunction = sum([q[lam] * sum([w[(s,a,lam)] * defenderSocialUtility(s,a,defenders, dCosts, dPenalties) for s in placements for a in attackerActions]) for lam in aTypes])
-        model2.add_constraints([sum([w[(s,a,lam)] * aUtility(s,a,lam, aPenalties, aRewards) for s in placements]) >= sum([w[(s,a,lam)] * aUtility(s,b,lam, aPenalties, aRewards) for s in placements]) for a in attackerActions for b in attackerActions if a != b for lam in aTypes], names=[f"c att {lam} suggested {a}, but goes to {b}" for a in attackerActions for b in attackerActions if a != b for lam in aTypes])
-        model2.add_constraints([sum([q[lam] * sum([w[dm,a,lam] * utilityM(d,dm,a,m, dRewards, dPenalties, dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) >= \
-                               sum([q[lam] * sum([w[dm,a,lam] * utilityM(e,dm,a,m, dRewards, dPenalties, dCosts) for a in attackerActions for dm in placements if dm[m] == d])]) \
-                               for m in defenders for d in range(targetNum + 1) for e in range(targetNum + 1) if d != e for lam in aTypes])
-        model2.add_constraints([sum([w[(s,a,lam)] for s in placements for a in attackerActions]) == 1 for lam in aTypes])
-        model2.maximize(objectiveFunction)
-        model2.solve()
-        baselineUtility += model2.solution.get_objective_value()
-        baselineTime += getTime() - baselineStart
+        # BP No Required Assignment
+        if not bpNRDAOverbudget:
+            bpNRDAStart = getTime()
+            bpNRDAUtility += solveBPNoRequiredDefenderAssignment(targetNum, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q)
+            bpNRDATime += getTime() - bpNRDAStart
+        else:
+            bpNRDAUtility = None
+            bpNRDATime = None
 
         # # Build the Baseline Model
-        # baselineStart = getTime()
-        # dStrat = {}
-        # models2 = {}
-        # for m in defenders:
-        #     model2 = Model(f"defenderStrategy{m}")
-        #     x = model2.continuous_var_list(keys=targetNum, lb=0, ub=1, name=f"x{m}")
-        #     h = model2.binary_var_dict(keys=[(lam, k) for lam in aTypes for k in range(targetNum)], lb=0, ub=1, name=f"h{m}")
-        #     ul = model2.continuous_var_dict(keys=aTypes, lb=-model2.infinity, name=f"ua{m}")
-        #     ud = model2.continuous_var_dict(keys=[lam for lam in aTypes], lb=-model2.infinity, name=f"ud{m}")
-        #     objectiveFunction = sum([q[lam] * ud[lam] for lam in aTypes])
-        #     model2.add_constraints([ud[lam] <= utilityDI(m,x,i,dRewards,dPenalties,dCosts) + (1-h[(lam,i)]) * M for i in range(targetNum) for lam in aTypes], names=[f"defender utility for lam {lam}, i {i}" for i in range(targetNum) for lam in aTypes])
-        #     model2.add_constraints([ul[lam] <= utilityLamI(x,lam,i,aRewards,aPenalties) + (1-h[(lam,i)]) * M for i in range(targetNum) for lam in aTypes], names=[f"lam {lam} utility leq for i {i}" for i in range(targetNum) for lam in aTypes])
-        #     model2.add_constraints([ul[lam] >= utilityLamI(x,lam,i,aRewards,aPenalties) for i in range(targetNum) for lam in aTypes], names=[f"lam {lam} utility geq, for i {i}" for i in range(targetNum) for lam in aTypes])
-        #     model2.add_constraints([sum([h[(lam,i)] for i in range(targetNum)]) == 1 for lam in aTypes], names=[f"h sum is 1 for lam {lam}" for lam in aTypes])
-        #     model2.add_constraint(sum([x[i] for i in range(targetNum)]) == 1)
-        #     # Solve the problem
-        #     model2.maximize(objectiveFunction)
-        #     model2.solve()
-        #     model2.export(f"modelBaseline{m}.lp")
-        #     dStrat[m] = list([float(xVal) for xVal in x])
-        #     models[m] = model2
-        # # Attacker response
-        # aStrat = 0
-        # protectionOdds = probabilityProtected(dStrat, targetNum)
-        # for lam in aTypes:
-        #     expectedUtilities = []
-        #     for i in range(targetNum):
-        #         expectedUtilities.append(((1-protectionOdds[i])*aRewards[lam][i]) + (protectionOdds[i]*aPenalties[lam][i]))
-        #     aStrat = argmax(expectedUtilities)
-        # for m in defenders:
-        #     baselineUtility += dStrat[m][aStrat] * (dRewards[m][aStrat] + dCosts[m][aStrat]) + (1-dStrat[m][aStrat]) * (dPenalties[m][aStrat] + dCosts[m][aStrat])
-        # baselineTime += getTime() - baselineStart
-    bpUtility /= avgCount
-    bpTime /= avgCount
-    baselineUtility /= avgCount
-    baselineTime /= avgCount
-    return bpUtility, bpTime, baselineUtility, baselineTime
+        if not baselineOverbudget:
+            baselineStart = getTime()
+            baselineUtility += solveBPNoRequiredDefenderAssignment(targetNum, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q)
+            baselineTime += getTime() - baselineStart
+        else:
+            baselineUtility = None
+            baselineTime = None
+    if not bpOverbudget:
+        bpUtility /= avgCount
+        bpTime /= avgCount
+    if not bpNRDAOverbudget:
+        bpNRDAUtility /= avgCount
+        bpNRDATime /= avgCount
+    if not baselineOverbudget:
+        baselineUtility /= avgCount
+        baselineTime /= avgCount
+    return bpUtility, bpTime, bpNRDAUtility, bpNRDATime, baselineUtility, baselineTime
 
 # ==============================================================================
 # GAME SETTINGS
 # ==============================================================================
-timeBudget = 30 * 60 # 30 minutes
-targetFloor = 3
-targetCeiling = 7
-defenderFloor = 1
-defenderCeiling = 4
-attackerFloor = 1
-attackerCeiling = 10
+timeBudget = 0.05 # 30 minutes
 DEFENDERNUM = 2
 ATTACKERNUM = 2
 TARGETNUM = 7
@@ -261,16 +294,32 @@ models = {}
 # ==============================================================================
 avgBPUtils = []
 avgBPTimes = []
+avgBPNRDAUtils = []
+avgBPNRDATimes = []
 avgBaselineUtils = []
 avgBaselineTimes = []
-for targetNum in range(targetFloor, targetCeiling + 1):
-    print(f"targetNum {targetNum} of {targetCeiling}")
-    bpUtility, bpTime, baselineUtility, baselineTime = getAvgUtilitiesAndTimes(targetNum, avgCount)
+# Iterate over the targets
+bpOver = False
+bpNRDAOver = False
+baselineOver = False
+for _ in range(3, 51):
+    print(f"Iteration {_} of {50} for targetCount")
+    bpUtility, bpTime, bpNRDAUtility, bpNRDATime, baselineUtility, baselineTime = iterateTargets(targetNum=_, avgCount=avgCount, bpOverbudget=bpOver, bpNRDAOverbudget=bpNRDAOver, baselineOverbudget=baselineOver)
+    if bpTime is not None and bpTime > timeBudget:
+        bpOver = True
+    if bpNRDATime is not None and bpNRDATime > timeBudget:
+        bpNRDAOver = True
+    if baselineTime is not None and baselineTime > timeBudget:
+        baselineOver = True
     avgBPUtils.append(bpUtility)
     avgBPTimes.append(bpTime)
+    avgBPNRDAUtils.append(bpNRDAUtility)
+    avgBPNRDATimes.append(bpNRDATime)
     avgBaselineUtils.append(baselineUtility)
     avgBaselineTimes.append(baselineTime)
+uGraph = createGraph("Average Utilities", "Number of Targets", "Utility", avgBPUtils, "BP Utility", avgBPNRDAUtils, "BPNRD Utility", avgBaselineUtils, "Baseline Utility", xStart = 3)
+tGraph = createGraph("Average Runtimes", "Number of Targets", "Runtime", avgBPTimes, "Persuasion Scheme Time", avgBPNRDATimes, "BPNRD Time", avgBaselineTimes, "Baseline Time", xStart = 3)
+# Iterate over the defenders
+# Iterate over the attackers
 
-uGraph = createGraph("Average Utilities", "Number of Targets", "Utility", avgBPUtils, "Persuasion Scheme Utility", avgBaselineUtils, "Baseline Utility")
-tGraph = createGraph("Average Runtimes", "Number of Targets", "Runtime", avgBPTimes, "Persuasion Scheme Time", avgBaselineTimes, "Baseline Time")
 plt.show()
