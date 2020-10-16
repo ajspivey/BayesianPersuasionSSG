d41766c9b433385914c102c9716aacffe3db3413
AJ Spivey <spivey.andrew@gmail.com>
2020-10-15 22:41:28 -0700

    Added calling module for the dual ellipsoid

---
+++ BPNONDDualEllipsoid.py
@@ -0,0 +1,28 @@
+# ==============================================================================
+# IMPORTS
+# ==============================================================================
+# External imports
+from docplex.mp.model import Model
+from itertools import permutations
+from functools import reduce
+from operator import mul
+from numpy import argmax
+from time import time as getTime
+import matplotlib.pyplot as plt
+import copy
+
+# Internal Imports
+from constants import RAND,DEFENDERNUM,ATTACKERNUM,TARGETNUM,AVGCOUNT,M
+from util import generateRandomDefenders, generateRandomAttackers, numberToBase, \
+                getPlacements, getOmegaKeys, defenderSocialUtility, utilityM, \
+                aUtility, getLambdaPlacements, utilityDI, utilityLamI, \
+                probabilityProtected, createGraph
+from gameTypes import solveBPAllowOverlap, solveBPNoRequiredDefenderAssignment, \
+                    solveBPNOOD, solveBPNOND, solveBaseline, solveBPNONDDualEllipsoid
+
+# ==============================================================================
+# FUNCTIONS
+# ==============================================================================
+defenders, dRewards, dPenalties, dCosts = generateRandomDefenders(DEFENDERNUM, TARGETNUM)
+aTypes, aRewards, aPenalties, q = generateRandomAttackers(ATTACKERNUM, TARGETNUM)
+score, model = solveBPNONDDualEllipsoid(TARGETNUM, defenders, dRewards, dPenalties, dCosts, aTypes, aRewards, aPenalties, q)
